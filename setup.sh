4e1f034c5fab254c8aeca20f8f4e43ceff873ee9
thinkernel <thinkernel@gmail.com>
2017-08-23 10:41:07 +0000

    Use DN directly instead of convert from domain

--- setup.sh
+++ setup.sh
@@ -3,16 +3,15 @@ set -e
 
 LAM_CONF_FILE=/var/www/html/lam/config/lam.conf
 
-#Convert FQDN to LDAP base DN
-SLAPD_TMP_DN=".${LDAP_DN}"
-while [ -n "${SLAPD_TMP_DN}" ]; do
-SLAPD_DN=",dc=${SLAPD_TMP_DN##*.}${SLAPD_DN}"
-SLAPD_TMP_DN="${SLAPD_TMP_DN%.*}"
-done
-SLAPD_DN="${SLAPD_DN#,}"
+: ${LDAP_URL:=ldap://openldap}
+: ${LDAP_PORT:=389}
+: ${SLAPD_DN:=dc=example,dc=com}
+: ${LDAP_ADMIN:=cn=admin,dc=example,dc=com}
+: ${USER_DN:=ou=people,dc=example,dc=com}
+: ${GROUP_DN:=ou=group,dc=example,dc=com}
 
 sed -i "s#serverURL: ldap://localhost:389#ServerURL: ${LDAP_URL}:${LDAP_PORT}#g" ${LAM_CONF_FILE}
-sed -i "s/admins: cn=Manager,dc=my-domain,dc=com/Admins: cn=admin,${SLAPD_DN}/g" ${LAM_CONF_FILE}
+sed -i "s/admins: cn=Manager,dc=my-domain,dc=com/Admins: ${LDAP_ADMIN}/g" ${LAM_CONF_FILE}
 sed -i "s/treesuffix: dc=yourdomain,dc=org/treesuffix: ${SLAPD_DN}/g" ${LAM_CONF_FILE}
 sed -i "s/types: suffix_user: ou=People,dc=my-domain,dc=com/types: suffix_user: ${USER_DN}/g" ${LAM_CONF_FILE}
 sed -i "s/types: suffix_group: ou=group,dc=my-domain,dc=com/types: suffix_group: ${GROUP_DN}/g" ${LAM_CONF_FILE}
